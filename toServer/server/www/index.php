91240ee6130e5788bb6d234012a8561db272278b
krystof <krystof@KT-PC>
2016-03-07 22:47:27 +0100

    version 6

--- toServer/server/www/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//require('.maintenance.php');
-
-$container = require __DIR__ . '/../app/bootstrap.php';
-
-$container->getByType('Nette\Application\Application')->run();
--- toServer/server/www/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-                $.nette.init();
-            });
-
-$(document).ready(function() {
-  $('.image').magnificPopup({type:'image'});
-});
